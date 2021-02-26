c271f9f041bd8aae62abe841ecff13e1fd4a37fd
John Edyson Mera <jemera@unitecnica.net>
2021-02-25 20:50:22 -0500

    year of birth
    new file

---
+++ anio.js
@@ -0,0 +1,5 @@
+alert("hola, voy a calcular tu año de nacimiento a partir de tu edad");
+let edad = prompt("Cual es tu edad?");
+let anho_actual = 2021;
+let anio_naci = anho_actual - edad;
+alert("Tu naciste " + anio_naci);

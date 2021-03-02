0acf8da615472d526b01e99d01eb160b0a91e27d
John Edyson Mera <jemera@unitecnica.net>
2021-03-01 19:24:38 -0500

    eighth

---
+++ taller1/ejercicio8.js
@@ -0,0 +1,17 @@
+// Juan quiere invitar a sus amigos al estadio para ver fútbol, las boletas cuestan 8000 pesos,
+//  pregúntele a Juan cuánto dinero tiene,  y muestre en pantalla cuántas personas puede invitar al estadio.
+// (ejemplo: si tiene 20.000 sólo puede invitar a 2 personas).
+
+alert("hola quieres entrar al estadio?");
+
+let = valor_boleta = 8000;
+let = valor_personas =0;
+
+while(valor_boleta<=8000){
+    valor_boleta += parseInt(prompt('Ingrese su saldo disponible'));
+    valor_personas++;
+}
+
+alert("el numero de personas que puede ingresar es ${valor_boleta}");
+alert("el total de ingresos es de : ${valor_disponible}");
+

fa34a2f83077f8360de1fcb4274c66a378b8eb04
John Edyson Mera <jemera@unitecnica.net>
2021-03-05 20:56:06 -0500

    continue

---
+++ taller3/ejercicio4.js
@@ -0,0 +1,9 @@
+/*
+ Leer dos números y mostrar todos los enteros comprendidos entre ellos.
+  dos numeros al usuario
+  mostrar enteros comprendidos entre ellos 
+
+ */
+
+  let num1 = parseInt(prompt("ingrese un numero"));
+  let num2 = parseInt(prompt("ingrese un otro numero"));

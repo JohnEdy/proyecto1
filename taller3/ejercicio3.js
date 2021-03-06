92b0959953a3bf5a12cd2504bf9d6fe9afa29805
John Edyson Mera <jemera@unitecnica.net>
2021-03-05 20:20:38 -0500

    new file

---
+++ taller3/ejercicio3.js
@@ -0,0 +1,16 @@
+/*. Leer un número entero y mostrar todos los divisores exactos del número comprendidos entre 1
+y el número leído.
+valor entrada = usuario
+1,2,4,8
+
+*/
+
+let numero = parseInt(prompt("Ingrese un numero para hallar por que numero es divisible"));
+
+
+for (let i = 1; i <= numero; i++) {
+     if (numero%i === 0 ) {
+         console.log(i);
+         }
+    }
+
